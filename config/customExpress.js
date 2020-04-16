1958a1a1ea878c7ba15ef2a17d347fd4c78d1cf8
thiago nunes <thiago-nunes-16@hotmail.com>
2020-04-16 11:37:38 -0300

    #1: Estuturando Path para Camada Controllers da Api

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,7 +1,12 @@
 var express = require('express');
+var consign = require('consign')
 
 module.exports = function() {
     var app = express();
+
+    consign()
+        .include('controllers')
+        .into();
 
     return app;
 }
--- index.js
+++ index.js
@@ -5,8 +5,3 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor Rodando na porta ${PORT}!`);
 })
-
-/* rota para testar se api está atendendo requisicoes.*/
-app.get("/teste", (req, res) => {
-    res.send(`Recebida requisicao de teste na porta ${PORT}.`);
-})
